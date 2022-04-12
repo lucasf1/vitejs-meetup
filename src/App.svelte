a2425bdfff058071bf9753990a9947b74067be51
lucasf1 <lucasf1@gmail.com>
2022-04-11 21:22:25 -0300

    2 files modified, 2 added

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,50 @@
 <script>
+  import Header  from './UI/Header.svelte';
+  import MeetupItem from './Meetups/MeetupItem.svelte';
 
+	const meetups = [
+		{
+			id: "m1",
+			title: "Coding Bootcamp",
+			subtitle: "Learn to code in 2 hours",
+			description:
+				"In this meetup, we will have some experts that teach you how to code!",
+			imageUrl:
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG/800px-Caffe_Nero_coffee_bar%2C_High_St%2C_Sutton%2C_Surrey%2C_Greater_London.JPG",
+			address: "27th Nerd Road, 32523 New York",
+			contactEmail: "code@test.com",
+			isFavorite: false,
+		},
+		{
+			id: "m2",
+			title: "Swim Together",
+			subtitle: "Let's go for some swimming",
+			description: "We will simply swim some rounds!",
+			imageUrl:
+				"https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Olympic_swimming_pool_%28Tbilisi%29.jpg/800px-Olympic_swimming_pool_%28Tbilisi%29.jpg",
+			address: "27th Nerd Road, 32523 New York",
+			contactEmail: "swim@test.com",
+			isFavorite: false,
+		},
+	];
 </script>
 
+<Header />
+
+<section id="meetups">
+  {#each meetups as meetup}
+    <MeetupItem 
+      title={meetup.title} 
+      subtitle={meetup.subtitle} 
+      description={meetup.description} 
+      imageUrl={meetup.imageUrl}
+      email={meetup.contactEmail}
+      address={meetup.address}/>
+  {/each}
+</section>
+
 <style>
-
+  #meetups {
+    margin-top: 5rem;
+  }
 </style>
-
-<h1>Time for a project!</h1>
